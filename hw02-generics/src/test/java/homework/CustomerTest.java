b98863baeb33af31f00edae3efffbce5fcfbf854
asologub61 <andrey-s-workmail@yandex.ru>
2023-01-26 23:17:29 +0300

    Add hw02-generics

--- hw02-generics/src/test/java/homework/CustomerTest.java
+++ hw02-generics/src/test/java/homework/CustomerTest.java
@@ -29,7 +29,6 @@ class CustomerTest {
     }
 
     @Test
-//    @Disabled //надо удалить
     @DisplayName("Объект Customer как ключ в карте")
     void customerAsKeyTest() {
         //given
@@ -56,7 +55,6 @@ class CustomerTest {
     }
 
     @Test
-//    @Disabled //надо удалить
     @DisplayName("Сортировка по полю score, итерация по возрастанию")
     void scoreSortingTest() {
         //given
@@ -99,7 +97,6 @@ class CustomerTest {
     }
 
     @Test
-//    @Disabled //надо удалить
     @DisplayName("Модификация коллекции")
     void mutationTest() {
         //given
@@ -121,7 +118,6 @@ class CustomerTest {
     }
 
     @Test
-//    @Disabled //надо удалить
     @DisplayName("Возвращание в обратном порядке")
     void reverseOrderTest() {
         //given
